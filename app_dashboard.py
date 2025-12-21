74c2abaa746219e153175db0666aa8c7b58d1369
Bruno Marinho <brunomarinho08@gmail.com>
2025-12-21 14:52:48 -0300

    Fix typo: ddef -> def em carregar_dados_planilha

--- app_dashboard.py
+++ app_dashboard.py
@@ -247,7 +247,7 @@ def mes_ano_ptbr_para_date(valor: str):
 
 
 @st.cache_data(ttl=60)
-def carregar_dados_planilha() -> pd.DataFrame:
+df carregar_dados_planilha() -> pd.DataFrame:
     """
     Carrega e trata a base da planilha do Google Sheets.
     Cache de 5 minutos.
